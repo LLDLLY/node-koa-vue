9d42af17ba1839f280e84148aff08634755abef9
yaoli <18861821692@163.com>
2020-12-25 15:13:42 +0800

    setState

--- src/store/common.js
+++ src/store/common.js
@@ -9,20 +9,20 @@ const state = {
 }
 
 const mutations = {
+  setState(state, data) {
+    if (!data || JSON.stringify(data) === "{}") {
+      return
+    }
+    if (Object.prototype.toString.call(data) !== "[object Object]") {
+      throw ('setState数据格式错误！')
+    }
+    const keys = Object.keys(data);
+    keys.forEach(item => {
+      state[item] = data[item]
+    })
+  },
   changeShow(state, data) {
     state.show = data
-  },
-  getHomeData(state, data) {
-    state.homeData = data
-  },
-  getGoodsDetail(state, data) {
-    state.goodsDetail = data
-  },
-  getGoodsComments(state, data) {
-    state.goodsComments = data
-  },
-  getGoodsCategories(state, data) {
-    state.goodsCategories = data;
   }
 }
 
@@ -30,26 +30,25 @@ const actions = {
   async getHomeDataFn({ commit }, data) {
     let res = await api.getHomeInitDataApi();
     if (res.code == 200) {
-      commit('getHomeData', res.data);
+      commit('setState', { homeData: res.data });
     }
   },
   async getGoodsDetailFn({ commit }, data) {
     let res = await api.getGoodsDetailApi(data);
     if (res.code == 200) {
-      // commit('mutation处理函数名字'，数据)
-      commit('getGoodsDetail', res.data);
+      commit('setState', { getGoodsDetail: res.data });
     }
   },
   async getGoodsCommentsFn({ commit }) {
     const res = await api.getGoodsCommentApi();
     if (res.code == 200) {
-      commit('getGoodsComments', res.data);
+      commit('setState', { getGoodsComments: res.data });
     }
   },
   async getGoodsCategoriesFn({ commit }) {
     const res = await api.getCategoriesApi();
     if (res.code == 200) {
-      commit('getGoodsCategories', res.data);
+      commit('setState', { getGoodsCategories: res.data });
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import user from './user'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules:{
+  modules: {
     common,
     user
   }
